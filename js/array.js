80241ed9308f06fa418824ed6e171181d9238fbe
Gianni Morgan <morgang823@gmail.com>
2021-04-15 13:54:31 -0400

    function update

--- js/array.js
+++ js/array.js
@@ -1,65 +1,66 @@
 function printNumbers() {
-    let start = parseInt(document.getElementById("numOne").value);
-    let end = parseInt(document.getElementById("numTwo").value);
-    let numbers = getRange(start, end);
-    displayData(numbers);
+    let startNum = parseInt(document.getElementById('numOne').value);
+    let endNum = parseInt(document.getElementById('numTwo').value);
+    let numFizz = parseInt(document.getElementById('numFizz').value);
+    let numBuzz = parseInt(document.getElementById('numBuzz').value);
+    let numFizzBuzz = numFizz * numBuzz;
+
+    let numbers = getRange(startNum, endNum);
+
+    displayData(numbers, numFizz, numBuzz, numFizzBuzz);
 }
-//Business type functionality, get the range of numbers
+
+// Gets the range of numbers
 function getRange(start, end) {
     let numberArray = [];
-    // Values[0,1,2,3,4,5,6,7,8,9]
-    //indexes [0,1,2,3,4,5,6,7,8]
-    //length = 10
-    //numberArray[0] == 1
-    for (let index = start; index <= end; index++) {
-        numberArray.push(index);
 
+    for (let i = start; i <= end; i++) {
+        numberArray.push(i);
     }
+
     return numberArray;
 }
 
-//Display Data Function
-function displayData(numbers) {
-    const rowTemplate = document.getElementById("Data-Template");
-    const resultsBody = document.getElementById("resultsBody");
-    let colCount = rowTemplate.content.cloneNode(true).querySelectorAll("td").length;
+// Displays the numbers on the page
+function displayData(numbers, fizz, buzz, fizzbuzz) {
+    const rowTemplate = document.getElementById('Data-Template');
+    const resultsBody = document.getElementById('resultsBody');
+    let colCount = rowTemplate.content.cloneNode('true').querySelectorAll('td')
+        .length;
 
-    //clear the table 
-    resultsBody.innerHTML = "";
+    resultsBody.innerHTML = ''; // Clear the table
 
-    // loop over every element in the numbers array
-    // get the value and write it to the page
-    for (let i = 0; i < numbers.length; i += colCount) {
-        let dataRow = rowTemplate.content.cloneNode(true);
-        let cols = dataRow.querySelectorAll("td");
+    // Loop over every element in the numbers array
+    for (let rowIndex = 0; rowIndex < numbers.length; rowIndex += colCount) {
+        // You need to clone it each time
+        let dataRow = rowTemplate.content.cloneNode('true');
+
+        // Return an array of columns
+        let cols = dataRow.querySelectorAll('td');
 
         for (let colIndex = 0; colIndex < cols.length; colIndex++) {
-            let value = numbers[i + colIndex]
-            if (typeof value === "undefined") {
-                value = "";
-            } else if (value % 3 == 0 && value % 15 != 0) {
-                cols[colIndex].classList.add("fizz")
-                value = "Fizz";
-            }
-            if (typeof value === "undefined") {
-                value = "";
-            } else if (value % 5 == 0 && value % 15 != 0) {
-                cols[colIndex].classList.add("buzz")
-                value = "Buzz";
+            let value = numbers[rowIndex + colIndex];
+
+            // FIZZBUZZ LOGIC
+            // Check if out of bounds
+            if (typeof value === 'undefined') {
+                value = '';
+            } else if (value % fizzbuzz === 0) {
+                value = `FizzBuzz (${value})`;
+                cols[colIndex].classList.add('fizzbuzz');
+            } else if (value % fizz === 0) {
+                value = `Fizz (${value})`;
+                cols[colIndex].classList.add('fizz');
+            } else if (value % buzz === 0) {
+                value = `Buzz (${value})`;
+                cols[colIndex].classList.add('buzz');
             }
 
-            if (typeof value === "undefined") {
-                value = "";
-            } else if (value % 15 == 0) {
-                cols[colIndex].classList.add("fizzBuzz")
-                value = "FizzBuzz";
-            }
-            // tds use textContent to set content
+            // TD value is textContent (not like other HTML tags that use value or innerHTML)
             cols[colIndex].textContent = value;
         }
 
-        // add the row to the page
+        // Inside the row loop (add row to the page)
         resultsBody.appendChild(dataRow);
     }
-
 }
